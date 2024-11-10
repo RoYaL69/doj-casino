2e343a21e03c49ea7367d69761f48be461211dbd
Marcell <chillcode.codersparadise@gmail.com>
2024-11-10 11:18:13 +0100

    Create de.lua

---
+++ casino-poker/locales/de.lua
@@ -0,0 +1,58 @@
+Locales['de'] = {
+    -- notifications
+    ['chair_occupied'] = 'Dieser Sitz ist besetzt.',
+    ['no_react'] = 'Du hast nicht rechtzeitig auf die Frage des Dealers reagiert und deine Karten abgelegt.',
+    ['no_bet_input'] = 'Du hast keinen Einsatz festgelegt.',
+    ['not_enough_chips'] = 'Du hast nicht genug Chips.',
+    ['not_enough_chips_next'] = 'Du hast nicht genug Chips für den Pair-Plus-Einsatz, da du nicht genug für das Spiel hättest.',
+    ['not_enough_chips_third'] = 'Du kannst diesen Einsatz nicht setzen, da du nicht genug für das Spielen deiner Hand hättest.',
+    ['not_enough_chips_toplay'] = 'Du hast nicht genug Chips, um zu spielen!',
+    ['already_betted']= 'Du hast bereits gesetzt.',
+    ['lose'] = 'Du verlierst!',
+    -- formatted notif
+    ['dealer_not_qual'] = 'Unentschieden.\nDer Dealer hat sich nicht für das Spiel qualifiziert.\nDu erhältst %s Chips zurück.',
+    ['dealer_not_qual_ante'] = 'Unentschieden.\nDer Dealer hat sich nicht für das Spiel qualifiziert.\nDu erhältst %s Chips zurück. (Ante Multiplikator: x%s)',
+    ['player_won_ante'] = 'Deine Hand gewinnt!\nDu erhältst %s Chips. (Ante Multiplikator: x%s)',
+    ['player_won'] = 'Deine Hand gewinnt!\nDu erhältst %s Chips.',
+    ['pair_won'] = 'Du gewinnst %s Chips mit deinem Pair-Plus-Einsatz! (Pair Multiplikator: x%s)',
+    -- hud
+    -- ['current_bet_input'] = 'BET:',
+    -- ['current_player_chips'] = 'CHIPS:',
+    -- ['table_min_max'] = 'MIN/MAX:',
+    -- ['remaining_time'] = 'TIME:',
+    -- top left
+    ['waiting_for_players'] = 'Warten auf Spieler...',
+    ['clearing_table'] = 'Tisch wird geleert...</br>Nächstes Spiel beginnt bald.',
+    ['dealer_showing_hand'] = 'Der Dealer zeigt seine Hand.',
+    ['players_showing_hands'] = 'Spielerhände werden aufgedeckt...',
+    ['dealing_cards'] = 'Karten werden an die Spieler ausgeteilt...',
+    -- inputs
+    ['fold_cards'] = 'Passen',
+    ['play_cards'] = 'Spielen',
+    -- ['leave_game'] = 'Leave game',
+    -- ['raise_bet'] = 'Raise bet',
+    -- ['reduce_bet'] = 'Reduce bet',
+    -- ['custom_bet'] = 'Custom bet',
+    -- ['place_bet'] = 'Place bet',
+    ['place_pair_bet'] = 'Pair-Plus-Einsatz platzieren',
+    -- gtao ui
+    -- ['tcp'] = '<C>[Aquiver]</C> ~b~Three Card Poker',
+    -- ['sit_down_table'] = '~h~<C>Play</C> ~b~Three Card Poker',
+    -- ['description'] = 'Game Description',
+    -- ['desc_1'] = 'TCP_DIS1', -- this is Rockstar Setuped default, this will automaticly change if you are using german language etc.
+    -- ['desc_2'] = 'TCP_DIS2',
+    -- ['desc_3'] = 'TCP_DIS3',
+    -- ['description_info'] = 'How is the game working?',
+    -- ['rule_1'] = 'TCP_RULE_1',
+    -- ['rule_2'] = 'TCP_RULE_2',
+    -- ['rule_3'] = 'TCP_RULE_3',
+    -- ['rule_4'] = 'TCP_RULE_4',
+    -- ['rule_5'] = 'TCP_RULE_5',
+    -- ['rule_header_1'] = 'TCP_RULE_1T',
+    -- ['rule_header_2'] = 'TCP_RULE_2T',
+    -- ['rule_header_3'] = 'TCP_RULE_3T',
+    -- ['rule_header_4'] = 'TCP_RULE_4T',
+    -- ['rule_header_5'] = 'TCP_RULE_5T',
+    -- ['rules'] = 'Game Rules',
+    -- ['rules_info'] = 'Game rules or anything you should know.'
+}
